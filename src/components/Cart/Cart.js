3905ac4e9ee5f942b8979a2c7c979387127fd8f7
rakibul58 <rhrahi14@gmail.com>
2022-09-27 14:31:43 +0600

    local storage added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from "react";
+import "./Cart.css";
 
-const Cart = ({cart}) => {
-   
-    return (
-        <div>
-            <h1>This order Summery</h1>
-            <p>Selected Items: {cart.length}</p>
-        </div>
-    );
+const Cart = ({ cart }) => {
+    
+    let total = 0;
+    let shipping = 0
+    for(const product of cart){
+        total += product.price;
+        shipping+=product.shipping;
+    }
+    const tax = total * 0.1;
+  return (
+    <div className="cart">
+      <h1>This order Summery</h1>
+      <p>Selected Items: {cart.length}</p>
+      <p>Total Price: ${total}</p>
+      <p>Total Shipping: ${shipping}</p>
+      <p>Tax: ${tax.toFixed(2)}</p>
+      <h5>Grand Total: {parseFloat(total+tax+shipping)}</h5>
+    </div>
+  );
 };
 
 export default Cart;
